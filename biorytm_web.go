8e2b7ef6de2d8d98dcb5e3f04d903e3f8b9e9cac
Adam Bryt <adam.bryt@gmx.com>
2015-03-06 22:17:46 +0100

    Przeniesienie templates do pliku template.go

--- biorytm_web.go
+++ biorytm_web.go
@@ -13,60 +13,6 @@ import (
 	"time"
 )
 
-// template dla formatki biorytmu tekstowego
-const formTextTmplStr = `
-<html>
-<head>
-	<title>Biorytm</title>
-</head>
-<body>
-	<p>
-	Podaj datę urodzenia i datę biorytmu w formacie yyyy-mm-dd
-	</p>
-
-	<form action="/text/biorytm/">
-		<table>
-			<tr>
-				<td style="text-align:right">data urodzenia:</td>
-				<td><input type="text" name="born"></td>
-			</tr>
-			<tr>
-				<td style="text-align:right">data aktualna:</td>
-				<td><input type="text" name="date" value="{{ .DateString }}"></td>
-			</tr>
-			<tr>
-				<td style="text-align:right">ilość dni:</td>
-				<td><input type="text" name="range" value="{{ .Days }}"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td style="text-align:right"><input type="submit"></td>
-			</tr>
-		</table>
-	</form>
-</body>
-</html>
-`
-
-// template dla wyniku biorytmu tekstowego
-const outputTextTmplStr = `
-<html>
-<head>
-	<title>Biorytm</title>
-	<!--
-	<style>
-		body {background-color:black; color:#FFC200}
-	</style>
-	-->
-</head>
-<body>
-	<p>
-	<pre>{{ . }}</pre>
-	</p>
-</body>
-</html>
-`
-
 var (
 	formTextTmpl   = template.New("formText")
 	outputTextTmpl = template.New("outputText")
